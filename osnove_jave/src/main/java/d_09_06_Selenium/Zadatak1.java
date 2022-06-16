400cb81bff2125a0f3caa17ad53414d955f0e7ec
Maja Pajkic <jomailic19@gmail.com>
2022-06-16 18:10:02 +0200

    Domaci 09.06.2022. ispravka

--- osnove_jave/src/main/java/d_09_06_Selenium/Zadatak1.java
+++ osnove_jave/src/main/java/d_09_06_Selenium/Zadatak1.java
@@ -7,7 +7,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
 import java.util.Scanner;
-
+//Maksimizirati prozor
+//Ucitati stranicu https://s.bootsnipp.com/iframe/WaXlr
+//Dohvatite dugmice za rejting kao listu. XPath za trazenje treba da bude preko id atributa i za ovo trebace vam contains u xpath-u
+//Od korisnika ucitati broj (preko scannera) na koju zvezdicu je potrebno kliknuti (u rasponu od 1 do 5)
+//I izvrsite akciju klik na odgovarajuci element preko indeksa
+//Na kraju programa ugasite pretrazivac.
 public class Zadatak1 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver",
@@ -15,12 +20,12 @@ public class Zadatak1 {
         Scanner s=new Scanner(System.in);
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();;
-        driver.get("https://s.bootsnipp.com/iframe/WaXlr");
+        driver.get("https://s.bootsnipp.com/iframe/WaXlr/");
         List<WebElement> dugmici=driver.findElements(By.xpath("//*[contains(@id, 'rating-star')]"));
-        System.out.println("Unesite broj od 1 do 5: ");
-        int n=s.nextInt();
+        System.out.println("Unesite broj zvezdica od 1 do 5: ");
+        int brojZvezdica=s.nextInt();
         for (int i = 0; i < dugmici.size(); i++) {
-           dugmici.get(n).click();
+           dugmici.get(brojZvezdica-1).click();
         }
         driver.quit();
 
--- osnove_jave/src/main/java/d_09_06_Selenium/Zadatak2.java
+++ osnove_jave/src/main/java/d_09_06_Selenium/Zadatak2.java
@@ -1,11 +1,26 @@
 package d_09_06_Selenium;
-//Napisati program koji vrsi dodavanje 5 reda u tabelu.
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+import java.util.List;
+
+//Napisati program koji vrsi dodavanje 5 reda u tabelu.
+//Maksimizirati prozor
+//Ucitati stranicu https://www.tutorialrepublic.com/snippets/bootstrap/table-with-add-and-delete-row-feature.php
+//Dodati 5 redova sa istim podacima.Jedan red u jednoj iteraciji
+//Klik na dugme Add New
+//Unesite name,departmant i phone (uvek iste vrednost)
+//Trazenje po name atributu
+//Kliknite na zeleno Add dugme.
+//PAZNJA: Pogledajte strukturu stranice i videcete da se u svakom redu poslednje kolone javljaju dugmici edit, add, delete ali zbog prirode reda neki dugmici se vide a neki ne.
+//Morate da dohvatite uvek Add dugme iz poslednjeg reda tabele. Mozete koristeci index iz petlje, a mozete koristeci i last() fukncionalnost za xpath. Koristan link last mehnizma
+//Cekanje od 0.5s
+//Na kraju programa ugasite pretrazivac.
 public class Zadatak2 {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver",
@@ -13,19 +28,27 @@ public class Zadatak2 {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.tutorialrepublic.com/snippets/bootstrap/table-with-add-and-delete-row-feature.php");
-        for (int i = 0; i < 5; i++) {
-            WebElement button=driver.findElement(By.xpath("//button[contains(@class, 'btn btn-info add-new')]"));
-            button.click();
-            WebElement input=driver.findElement(By.xpath("//table[contains(@class, 'table table-bordered')]/tbody/tr[4]/td/input"));
-            input.sendKeys("Iva Ivic");
-            WebElement input1=driver.findElement(By.xpath("//table[contains(@class, 'table table-bordered')]/tbody/tr[4]/td[2]/input"));
-            input1.sendKeys("Quality Assurance");
-            WebElement input2=driver.findElement(By.xpath("//table[contains(@class, 'table table-bordered')]/tbody/tr[4]/td[3]/input"));
-            input2.sendKeys("(517)456-789");
-            WebElement button1=driver.findElement(By.xpath("//table[contains(@class, 'table table-bordered')]/tbody/tr[4]/td[4]/a/i"));
-            button1.click();
+
+        for (int i = 0; i < 5 ; i++) {
+            driver
+                    .findElement(By.className("add-new"))
+                    .click();
+            driver
+                    .findElement(By.name("name"))
+                    .sendKeys("Iva Ivic");
+            driver
+                    .findElement(By.name("department"))
+                    .sendKeys("Quality Assurance");
+            driver
+                    .findElement(By.name("phone"))
+                    .sendKeys("(517)456-789");
+            driver
+                    .findElements(By.xpath("//a[@class='add']")).get(3+i)
+                    .click();
             Thread.sleep(500);
         }
-        driver.quit();
+            driver.quit();
+        }
     }
-}
+
+
